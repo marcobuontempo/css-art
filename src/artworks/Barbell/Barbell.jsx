7ee5d00024c568ad5a2563207b061e2059e291dc
marcobuontempo <marco.buon36@gmail.com>
2023-09-06 23:07:53 +1000

    Complete 'Barbell' artwork

---
+++ src/artworks/Barbell/Barbell.jsx
@@ -0,0 +1,10 @@
+import TRAITS from '../TRAITS'
+import './Barbell.css'
+
+export default function Barbell() {
+  return <div className='barbell'></div>
+}
+
+Barbell.artname = "Barbell.";
+Barbell.frameColour = "black";
+Barbell.traits = [TRAITS.ONEDIV];
