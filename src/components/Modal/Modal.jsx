8bd5c3f2baf4c747a67323adc773ef820dfa8a7e
marcobuontempo <marco.buon36@gmail.com>
2023-11-14 18:11:16 +1100

    Implement Github links in Modal window for each Artwork

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,6 +1,9 @@
 import tinycolor from "tinycolor2";
 import ArtFrame from "../ArtFrame/ArtFrame";
 import "./Modal.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFileCode } from "@fortawesome/free-regular-svg-icons";
+import { faCss3Alt } from "@fortawesome/free-brands-svg-icons";
 
 export default function Modal({ children, visible, setVisible, isArtwork = false, Artwork }) {
   const closeModal = (e) => {
@@ -9,12 +12,29 @@ export default function Modal({ children, visible, setVisible, isArtwork = false
   };
 
   if (isArtwork === true) {
-    const frameIsLight = tinycolor(Artwork.frameColour).isLight();
+    const mainColour = tinycolor(Artwork.frameColour).isLight() ? "white" : "black";
+    const contrastColour = mainColour === "white" ? "black" : "white";
+    const formattedArtname = Artwork.artname.replace(/[^a-zA-Z]/g, "");
+    const githubLinks = {
+      jsx: `https://github.com/marcobuontempo/css-art/tree/main/src/artworks/${formattedArtname}/${formattedArtname}.jsx`,
+      css: `https://github.com/marcobuontempo/css-art/tree/main/src/artworks/${formattedArtname}/${formattedArtname}.css`,
+
+    }
     return (
       <div className="modal modal__artwork" style={{ display: visible ? "flex" : "none" }}>
         <div className="modal__main"
           style={{ "--frame-colour": Artwork.frameColour }}>
-          <h2 className="modal__title" style={{ color: frameIsLight ? "black" : "white" }}>{Artwork.artname}</h2>
+          <div className="modal__head">
+            <h2 className="modal__title" style={{ color: contrastColour }}>{Artwork.artname}</h2>
+            <div className="modal__artwork_gh">
+              <a target="_blank" rel="noreferrer" href={githubLinks.jsx}>
+                <FontAwesomeIcon icon={faFileCode} size="2x" color={contrastColour} />
+              </a>
+              <a target="_blank" rel="noreferrer" href={githubLinks.css}>
+                <FontAwesomeIcon icon={faCss3Alt} size="2x" color={contrastColour} />
+              </a>
+            </div>
+          </div>
           <ArtFrame
             key={Artwork.name}
             canvasColour={Artwork.canvasColour}
@@ -24,7 +44,7 @@ export default function Modal({ children, visible, setVisible, isArtwork = false
             traits={Artwork.traits}
             isModal={true}
           />
-          <button className="modal__close" onClick={closeModal}>Close</button>
+          <button className="modal__close" onClick={closeModal} style={{ color: mainColour, border: `2px solid ${mainColour}`, background: contrastColour }}>Close</button>
         </div>
       </div>
     );
